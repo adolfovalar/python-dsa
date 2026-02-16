0f68e67f4f5912c76b43833044719cdd5d11e970
adolfovalar <adolfovalar@gmail.com>
2026-02-16 16:28:45 -0600

    respuesta fácil de two sum

--- arrays_hashing/two_sum.py
+++ arrays_hashing/two_sum.py
@@ -1,1 +1,8 @@
-#first commit
+nums = [2,7,11,15]
+target = 9
+
+for n1 in nums:
+    for n2 in nums:
+        if n1 + n2 == target:
+            print(f"{n1} + {n2} son los números necesarios")
+        pass
