29e18968d5dcbce984f36b9361f35c9453a4ffc9
adolfovalar <adolfovalar@gmail.com>
2026-02-17 10:03:18 -0600

    solución a two sum optimizada

--- arrays_hashing/two_sum_optimal.py
+++ arrays_hashing/two_sum_optimal.py
@@ -1,13 +1,27 @@
+"""
+Problem: Two Sum
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
 nums = [2,7,11,15]
 target = 9
 vistos = {}
 
+# Transformar lista en diccionario. El n1 será la clave. 
 for index, n1 in enumerate(nums):
+    #calcular número necesario
     num_necesario = target - n1
-    print(f'el número necesario es {num_necesario}')
-
+    print(f'El número necesario es {num_necesario}')
+    #buscar número necesario en diccionario vistos
     if num_necesario in vistos:
-        print(f'los números son {n1} y {num_necesario}')
+        print(f'¡Se encontró el número necesario!')
+        print(f'Los números que suman {target} son {n1} y {num_necesario}')
         break
+    else:
+        print(f'Todavía no hay ningún {num_necesario} en el diccionario')
+        #Agremos número iterado a diccionario
+        vistos[n1] = index
+        print(f'El diccionario contiene{vistos}')
 
 
