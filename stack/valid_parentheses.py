1bc81476bd953ce974745e08c3dd98e83fc3e92a
adolfovalar <adolfovalar@gmail.com>
2026-02-18 00:44:04 -0600

    avance en valid parentheses

--- stack/valid_parentheses.py
+++ stack/valid_parentheses.py
@@ -1,2 +1,20 @@
-s = ''
+s = '())[]{}'
 stack = []
+dict_caracteres = {')':'(', ']':'[','}':'{'}
+
+# Guardar carácteres en el stack.
+for caracter in s:
+    if caracter in dict_caracteres and len(stack) > 0:
+        if dict_caracteres[caracter] == stack[-1]:
+            stack.pop()
+            print(stack)
+        else: 
+            break
+    else:
+        stack.append(caracter)
+        print(stack)
+
+if len(stack) > 0:
+    print('No es válido.')
+
+
